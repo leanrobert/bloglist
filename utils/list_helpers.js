a8bb96ca5101254f6068111c83d31e259b80ca63
leanrobert <53313456+leanrobert@users.noreply.github.com>
2021-09-07 14:26:31 -0300

    part 4.5

--- utils/list_helpers.js
+++ utils/list_helpers.js
@@ -6,9 +6,20 @@ const totalLikes = (blogs) => {
     if(blogs.length === 0) {
         return 0
     }
-    
+
     const total = blogs.map(blog => blog.likes).reduce((acc, likes) => acc + likes)
     return total
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+    if(blogs.length === 0) {
+        return null
+    }
+
+    const likes = blogs.map(blog => blog.likes)
+    const maxLikes = Math.max(...likes)
+    maxBlog = blogs.find(blog => blog.likes === maxLikes && blog)
+    return maxBlog
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
