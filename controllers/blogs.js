2620b9ec3585523cdbdc2a7a0deb53cc614fab14
leanrobert <53313456+leanrobert@users.noreply.github.com>
2021-09-08 15:25:11 -0300

    part 4.13 and 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,26 +1,39 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (req, res) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            res.json(blogs)
-        })
+blogsRouter.get('/', async (req, res) => {
+    const blogs = await Blog.find({})
+    res.json(blogs)
 })
 
-blogsRouter.post('/', (req, res) => {
+blogsRouter.post('/', async (req, res) => {
     const blog = new Blog(req.body)
 
     if(!blog.title || !blog.url) {
         return res.status(400).send({ error: 'Title or url missing' })
     }
 
-    blog
-        .save()
-        .then(result => {
-            res.status(201).json(result)
-        })
+    const savedblog = await blog.save()
+    res.status(201).json(savedblog)
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+    const body = req.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, blog, { new: true })
+    res.json(updatedBlog)
+})
+
+blogsRouter.delete('/:id', async (req, res) => {
+    await Blog.findByIdAndRemove(req.params.id)
+    res.status(204).end()
 })
 
 module.exports = blogsRouter
