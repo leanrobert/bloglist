3ffc334803c8fcb8a0c067491b15e755817f9f15
leanrobert <53313456+leanrobert@users.noreply.github.com>
2021-09-09 11:26:32 -0300

    part 4.18

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,19 +1,33 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     res.json(blogs)
 })
 
 blogsRouter.post('/', async (req, res) => {
-    const blog = new Blog(req.body)
+    const body = req.body
+
+    const user = await User.findById(body.userId)
+    
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
 
     if(!blog.title || !blog.url) {
         return res.status(400).send({ error: 'Title or url missing' })
     }
 
     const savedblog = await blog.save()
+    user.blogs = user.blogs.concat(savedblog._id)
+    await user.save()
+
     res.status(201).json(savedblog)
 })
 
